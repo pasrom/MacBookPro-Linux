f9259941580a2465b54ea49c8dee4199eb14d9dc
pasrom <32713472+pasrom@users.noreply.github.com>
2019-03-24 10:32:47 +0100

    force yes

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -2,20 +2,20 @@
 
 sudo apt-get update && sudo apt-get upgrade -y
 
-sudo apt-get install git chromium htop caja-open-terminal
+sudo apt-get install -y git chromium htop caja-open-terminal
 
 # wlan driver BCM4360
-sudo apt-get install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') \
+sudo apt-get install -y linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') \
   linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') \
   broadcom-sta-dkms
 
 # sensors
-sudo apt install lm-sensors hddtemp mate-sensors-applet mbpfan
+sudo apt install -y lm-sensors hddtemp mate-sensors-applet mbpfan
 sudo cp config/mbpfan.conf /etc/mbpfan.conf
 sudo systemctl restart mbpfan.service
 
 # kernel
-sudo apt install bison flex libelf-dev libssl-dev debhelper dh-virtualenv \
+sudo apt install -y bison flex libelf-dev libssl-dev debhelper dh-virtualenv \
   build-essential libssl-dev libncurses5-dev bison flex bc qt5-default \
   qt5-qmake qtbase5-dev qtbase5-dev-tools pkg-config ccache
 
